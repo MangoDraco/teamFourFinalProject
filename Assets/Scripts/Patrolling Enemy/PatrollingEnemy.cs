59d4ca9b58f06bc5fdfe0adcc64d77e514b14886
Dylan/Nyx <163457812+soulxria@users.noreply.github.com>
2025-04-08 13:52:50 -0400

    patrolling enemy properly added w folder :3

---
+++ Assets/Scripts/Patrolling Enemy/PatrollingEnemy.cs
@@ -0,0 +1,100 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Apple;
+
+public class PatrollingEnemy : MonoBehaviour
+{
+
+    private Rigidbody rB;   
+
+
+    //patrol mode variables
+    public Transform[] patrolPoints;
+
+    private NavMeshAgent enemyAgent;
+    public Transform target;
+    private Animator enemyAnimator;
+    private int targetPoint;
+
+    public GameObject player;
+
+    //enemy sound variables
+    AudioSource audioSource;
+    public AudioClip footstepsWalk;
+    private bool walking;
+
+
+
+
+
+    void Start()
+    {
+        rB = GetComponent<Rigidbody>();
+        enemyAgent = GetComponent<NavMeshAgent>();
+        enemyAnimator = GetComponent<Animator>();
+        targetPoint = Random.Range(0, 6);
+        enemyAgent.speed = 2.5f;
+        audioSource = GetComponent<AudioSource>();
+
+        enemyAgent.SetDestination(patrolPoints[targetPoint].position);
+        audioSource.loop = true;
+        walking = true;
+    }
+
+    void Update ()
+    {
+        
+        //keep track of sound states
+        soundSpeedController();
+        //once timer hits zero, set chaseOn true and call chasemode
+
+        if (Vector3.Distance(enemyAgent.transform.position, patrolPoints[targetPoint].position) < 0.5f)
+        {
+            Debug.Log("Target hit, changing");
+            targetPoint = changeTargetInt();
+            SetPosition();
+        }
+
+    }
+
+    void soundSpeedController()
+    {
+        float noiseTimer = 0.0f;
+
+        if (walking && noiseTimer <= 0)
+        {
+            PlaySoundOnce(footstepsWalk);
+            noiseTimer = 2.0f;
+            noiseTimer -= Time.deltaTime;
+        }
+    }
+
+    void SetPosition()
+    {
+        enemyAgent.SetDestination(patrolPoints[targetPoint].position);
+    }
+    int changeTargetInt()
+    {
+        int newVal = Random.Range(0,6);
+        return newVal;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            enemyAgent.isStopped = true;
+        }
+    }
+
+   
+    public void PlaySoundOnce(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+}
+
+
