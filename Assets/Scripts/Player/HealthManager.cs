fee04a62181aaa0c775c7705f46d561d1a060069
Dylan/Nyx <163457812+soulxria@users.noreply.github.com>
2025-04-10 15:15:58 -0400

    pushing checkpoints prototype script and health manager

---
+++ Assets/Scripts/Player/HealthManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public int maxHealth;
+    public int curHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (curHealth == 0)
+        {
+            Death();
+        }
+    }
+
+    void TakeDmg(int dmg)
+    {
+        if (curHealth > 0)
+        {
+            curHealth -= dmg;
+        }
+        else
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+        Debug.Log("You have died");
+    }
+
+    bool Heal()
+    {
+        if(curHealth == maxHealth)
+        {
+            return false;
+        }
+        else
+        {
+            curHealth += 1;
+            return true;
+        }
+    }
+
+    //Health Pickup Manager
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Health Pickup")
+        {
+            bool charHealed = Heal();
+            if (charHealed)
+            {
+                Destroy(collision.gameObject);
+                charHealed = false;
+            }
+
+        }
+    }
+}
