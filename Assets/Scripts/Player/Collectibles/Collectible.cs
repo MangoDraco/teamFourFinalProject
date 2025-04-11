f5d2381ce23375861d40f96bb6dc77b56a620078
aknez615 <al683193@ucf.edu>
2025-04-11 14:25:08 -0400

    powerup scripts

--- Assets/Scripts/Player/Collectibles/Collectible.cs
+++ Assets/Scripts/Player/Collectibles/Collectible.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collectible : MonoBehaviour
+namespace teamFourFinalProject
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Collectible : Entity
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
