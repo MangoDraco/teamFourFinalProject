661425830b0d2c960dc994dd1e352cae7d56d0a1
aknez615 <al683193@ucf.edu>
2025-04-11 14:10:54 -0400

    platform objects

--- Assets/Scripts/Platforms/PlatformCollisionHandler.cs
+++ Assets/Scripts/Platforms/PlatformCollisionHandler.cs
@@ -6,6 +6,28 @@ namespace teamFourFinalProject
 {
     public class PlatformCollisionHandler : MonoBehaviour
     {
+        Transform platform; //The platform, if any, we are on top of it
 
+        void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.CompareTag("MovingPlatform"))
+            {
+                //If contact normal is pointing up, we've collided with the top of the platform
+                ContactPoint contact = other.GetContact(index: 0);
+                if (contact.normal.y < 0.5f) return;
+
+                platform = other.transform;
+                transform.SetParent(platform);
+            }
+        }
+
+        void OnCollisionExit(Collision other)
+        {
+            if (other.gameObject.CompareTag("MovingPlatform"))
+            {
+                transform.SetParent(p: null);
+                platform = null;
+            }
+        }
     }
 }
