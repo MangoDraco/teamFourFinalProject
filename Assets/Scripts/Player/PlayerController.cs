5a7a15cf7c160e99fd4a55b2a998a75a41158422
aknez615 <al683193@ucf.edu>
2025-04-10 23:28:17 -0400

    jumping

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -14,6 +14,7 @@ namespace teamFourFinalProject
         [Header("References")]
         [SerializeField, Self] Animator animator;
         [SerializeField, Self] Rigidbody rb;
+        [SerializeField, Self] GroundChecker groundChecker;
         [SerializeField, Anywhere] CinemachineFreeLook freelookVCam;
         [SerializeField, Anywhere] InputReader input;
 
@@ -22,17 +23,29 @@ namespace teamFourFinalProject
         [SerializeField] float rotationSpeed = 15f;
         [SerializeField] float smoothTime = 0.2f;
 
+        [Header("Jump Settings")]
+        [SerializeField] float jumpForce = 10f;
+        [SerializeField] float jumpDuration = 0.5f;
+        [SerializeField] float jumpCooldown = 0f;
+        [SerializeField] float jumpMaxHeight = 2f;
+        [SerializeField] float gravityMultiplier = 3f;
+
         //Animator
         static readonly int Speed = Animator.StringToHash(name: "Speed");
 
         float currentSpeed;
         float velocity;
+        float jumpVelocity;
 
         Transform mainCam;
 
         const float ZeroF = 0f;
 
         Vector3 movement;
+
+        List<JumpTimer> timers;
+        CountdownTimer jumpTimer;
+        CountdownTimer jumpCooldownTimer;
 
         private void Awake()
         {
@@ -44,6 +57,13 @@ namespace teamFourFinalProject
             freelookVCam.OnTargetObjectWarped(transform, positionDelta: transform.position - freelookVCam.transform.position - Vector3.forward);
 
             rb.freezeRotation = true;
+
+            //Setting up timers
+            jumpTimer = new CountdownTimer(jumpDuration);
+            jumpCooldownTimer = new CountdownTimer(jumpCooldown);
+            timers = new List<JumpTimer>(capacity:2) { jumpTimer, jumpCooldownTimer };
+
+            jumpTimer.OnTimerStop += () => jumpCooldownTimer.Start();
         }
 
         private void Start()
@@ -51,17 +71,90 @@ namespace teamFourFinalProject
             input.EnablePlayerActions();
         }
 
+        void OnEnable()
+        {
+            input.Jump += OnJump;
+        }
+
+        void OnDisbale()
+        {
+            input.Jump -= OnJump;
+        }
+
+        void OnJump(bool performed)
+        {
+            if (performed && !jumpTimer.isRunning && !jumpCooldownTimer.isRunning && groundChecker.isGrounded)
+            {
+                jumpTimer.Start();
+            }
+
+            else if (!performed && jumpTimer.isRunning)
+            {
+                jumpTimer.Stop();
+            }
+        }
+
         private void Update()
         {
             movement = new Vector3(input.Direction.x, y: 0f, z: input.Direction.y);
 
             HandleAnimator();
+            HandleTimers();
         }
 
         private void FixedUpdate()
         {
-            //HandleJump();
+            HandleJump();
             HandleMovement();
+        }
+        void HandleAnimator()
+        {
+            animator.SetFloat(id: Speed, currentSpeed);
+        }
+
+        void HandleTimers()
+        {
+            foreach (var timer in timers)
+            {
+                timer.Tick(Time.deltaTime);
+            }
+        }
+
+        void HandleJump()
+        {
+            //If not jumping and grounded, keep jump velocity at 0
+            if (!jumpTimer.isRunning && groundChecker.isGrounded)
+            {
+                jumpVelocity = ZeroF;
+                jumpTimer.Stop();
+                return;
+            }
+
+            //If jumping or falling calculate velocity
+            if (jumpTimer.isRunning)
+            {
+                //Progress point for inital burst of velocity
+                float launchPoint = 0.9f;
+                if (jumpTimer.Progress > launchPoint)
+                {
+                    //Calculate the velocity required to reach the jump height using physics equations v = sqrt(2gh) (height (h), gravity (g), velocity (v)
+                    jumpVelocity = Mathf.Sqrt(f: 2 * jumpMaxHeight * Mathf.Abs(Physics.gravity.y));
+                }
+
+                else
+                {
+                    //Gradually apply less velocity as the jump progresses
+                    jumpVelocity += (1 - jumpTimer.Progress) * jumpForce * Time.fixedDeltaTime;
+                }
+            }
+
+            else
+            {
+                jumpVelocity += Physics.gravity.y * gravityMultiplier * Time.fixedDeltaTime;
+            }
+
+            //Apply Velocity
+            rb.velocity = new Vector3(rb.velocity.x, jumpVelocity, rb.velocity.z);
         }
 
         void HandleMovement()
@@ -81,11 +174,6 @@ namespace teamFourFinalProject
                 //Reset horizontal velocity
                 rb.velocity = new Vector3(x: ZeroF, rb.velocity.y, z: ZeroF);
             }
-        }
-
-        void HandleAnimator()
-        {
-            animator.SetFloat(id: Speed, currentSpeed);
         }
 
         void HandleHorizontalMovement(Vector3 adjustedDirection)
