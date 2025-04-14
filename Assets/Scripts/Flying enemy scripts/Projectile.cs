5b0c15b482ab0e1777ef0e7ad094730ed4949d78
Tom-Dean <Thomas.dean.iii1002@gmail.com>
2025-04-14 15:35:02 -0400

    stuff

--- Assets/Scripts/Flying enemy scripts/Projectile.cs
+++ Assets/Scripts/Flying enemy scripts/Projectile.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public AudioSource src;
+    public AudioClip impact;
+
     public void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
+            src.PlayOneShot(impact);
             Destroy(gameObject);
         }
     }
+
 }
