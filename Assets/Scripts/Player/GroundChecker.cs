e3994e1a19af03a3287580292cdb9960b8f948e0
aknez615 <al683193@ucf.edu>
2025-04-20 19:42:55 -0400

    platforms

--- Assets/Scripts/Player/GroundChecker.cs
+++ Assets/Scripts/Player/GroundChecker.cs
@@ -17,24 +17,18 @@ namespace teamFourFinalProject
 
         private void Update()
         {
+            Vector3 origin = transform.position + Vector3.up * 0.1f;
+
             isGrounded = Physics.SphereCast(origin: transform.position, radius: groundDistance, direction: Vector3.down, out _, groundDistance, (int)groundLayers);
             isCardGrounded = Physics.SphereCast(origin: transform.position, radius: groundDistance, direction: Vector3.down, out _, groundDistance, cardLayers);
         }
 
         private void OnDrawGizmos()
         {
-            Vector3 origin = transform.position;
-            Vector3 direction = transform.forward;
-            float radius = 0.5f;
-            float maxDistance = 10f;
-
-            Gizmos.color = Color.green;
-            Gizmos.DrawWireSphere(origin, radius);
-            Gizmos.DrawWireSphere(origin + direction * maxDistance, radius);
-            Gizmos.DrawLine(origin + Vector3.up * radius, origin + direction * maxDistance + Vector3.up * radius);
-            Gizmos.DrawLine(origin - Vector3.up * radius, origin + direction * maxDistance - Vector3.up * radius);
-            Gizmos.DrawLine(origin + Vector3.right * radius, origin + direction * maxDistance + Vector3.right * radius);
-            Gizmos.DrawLine(origin - Vector3.right * radius, origin + direction * maxDistance - Vector3.right * radius);
+            Gizmos.color = Color.yellow;
+            Vector3 origin = transform.position + Vector3.up * 0.1f;
+            Gizmos.DrawWireSphere(origin, groundDistance);
+            Gizmos.DrawWireSphere(origin + Vector3.down * (groundDistance + 0.1f), groundDistance);
         }
     }
 }
