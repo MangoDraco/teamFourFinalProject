30305cf90c0ad50f7350240b71b6cc57bc19b93c
aknez615 <al683193@ucf.edu>
2025-04-21 01:59:01 -0400

    SceneManager

--- Assets/Scripts/DataPersistence/Data/GameData.cs
+++ Assets/Scripts/DataPersistence/Data/GameData.cs
@@ -8,11 +8,19 @@ using UnityEngine;
         public int curHealth;
         public int playerCheckpoint;
 
+        public int currentLevel;
+        public int unlockedLevels;
+
+        public bool[] keysCollected;
+
         //the values defined in this will be default values for when game loads in
 
         public GameData()
         {
-            this.curHealth = 5;
-            this.playerCheckpoint = 0;
+            this.curHealth = 5; //Player health
+            this.playerCheckpoint = 0; //Checkpoints reached during levels
+            this.currentLevel = 0; //HUB
+            this.unlockedLevels = 1; //Level 1 is unlocked
+            this.keysCollected = new bool[3]; //3 Keys
         }
     }
--- Assets/Scripts/Game/SceneManager.cs
+++ Assets/Scripts/Game/SceneManager.cs
@@ -1,14 +1,82 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
+public class SceneManager : MonoBehaviour, IDataPersistence
 {
-    
+    public static SceneManager instance;
+
+    public int currentLevel = 0;
+    public int unlockedLevels = 1;
+    public bool[] keysCollected = new bool[3];
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     public void LoadNextScene(string sceneName)
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
+
+    public void LoadLevel(int levelIndex)
+    {
+        if (levelIndex < unlockedLevels)
+        {
+            currentLevel = levelIndex;
+            UnityEngine.SceneManagement.SceneManager.LoadScene(levelIndex);
+        }
+
+        else
+        {
+            Debug.Log("level " + levelIndex + " is locked");
+        }
+    }
+
+    public void CompleteLevel(int levelIndex)
+    {
+        if (levelIndex < keysCollected.Length)
+        {
+            keysCollected[levelIndex] = true;
+
+            if (unlockedLevels < levelIndex + 2 && levelIndex + 1 < keysCollected.Length)
+            {
+                unlockedLevels = levelIndex + 2;
+            }
+
+            if (AllKeysCollected())
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("youWin");
+            }
+
+            else
+            {
+                LoadLevel(0); //Return to HUB
+            }
+        }
+    }
+
+    public bool AllKeysCollected()
+    {
+        foreach (bool collected in keysCollected)
+        {
+            if (!collected) return false;
+        }
+
+        return true;
+    }
+
     public void QuitGame()
     {
         Application.Quit();
@@ -21,5 +89,19 @@ public class SceneManager : MonoBehaviour
             Application.Quit();
             Debug.Log("The game has closed");
         }
+    }
+
+    public void LoadData(GameData data)
+    {
+        currentLevel = data.currentLevel;
+        unlockedLevels = data.unlockedLevels;
+        keysCollected = data.keysCollected ?? new bool[3];
+    }
+
+    public void SaveData(ref GameData data)
+    {
+        data.currentLevel = currentLevel;
+        data.unlockedLevels = unlockedLevels;
+        data.keysCollected = keysCollected;
     }
 }
