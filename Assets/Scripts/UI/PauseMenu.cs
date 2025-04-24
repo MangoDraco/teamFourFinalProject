fe4bad289ea624c01e9e405adbba1546fd89a480
MangoDraco <le303029@ucf.edu>
2025-04-24 13:27:51 -0400

    finalBuildv1

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -10,7 +10,17 @@ public class PauseMenu : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
 
     public void Pause()
